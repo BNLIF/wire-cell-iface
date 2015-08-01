e91d7caa3f56e081f7919efa8cf9d34ae6f54d59
Brett Viren <brett.viren@gmail.com>
2015-08-01 14:03:47 -0400

    Allow for holding link back to a previous deposition and move comparitor to IDepo.h

--- inc/WireCellIface/IDepo.h
+++ inc/WireCellIface/IDepo.h
@@ -4,8 +4,12 @@
 #include "WireCellIface/ISequence.h"
 
 #include "WireCellUtil/Point.h"
+#include "WireCellUtil/Units.h"
 
 namespace WireCell {
+
+    class IDepo;
+    typedef std::shared_ptr<const IDepo> IDepoPtr;
 
     /** An interface to information about a deposition of charge.
      */
@@ -14,7 +18,7 @@ namespace WireCell {
 	virtual ~IDepo() {};
 
 	/// The location of the deposition.
-	virtual Point point() const = 0;
+	virtual const Point& pos() const = 0;
 
 	/// The number of seconds from some absolute start time to
 	/// when the deposition occur ed.
@@ -23,6 +27,9 @@ namespace WireCell {
 	/// The number charge (in units of number of electrons) deposited.
 	virtual double charge() const = 0;
 	
+	/// If the deposition is drifted, this accesses the original.
+	virtual IDepoPtr original() const { return 0; }
+
     };
 
 }
@@ -34,12 +41,25 @@ WIRECELL_SEQUENCE_SINK(Depo,depo);
 
 namespace WireCell {
 
-    /** In addition to the abstract base iterator access directly to a
-     * pointer to IDepo, it is helpful to have a share_ptr. */
-    typedef std::shared_ptr<const IDepo> IDepoPtr;
     typedef IteratorBase<IDepoPtr> depoptr_base_iterator;
     typedef Iterator<IDepoPtr> depoptr_iterator;
-    typedef std::pair< depoptr_iterator, depoptr > depoptr_range;
+    typedef std::pair< depoptr_iterator, depoptr_iterator > depoptr_range;
+
+    /// Compare how "far" two depositions are from the origin along
+    /// the drift-line (metric: dT + dX/V_drift) given a drift velocity.
+    class IDepoPtrDriftCompare {
+	double m_drift_velocity;
+    public:
+	IDepoPtrDriftCompare(double drift_velocity = 1.6 *units::mm/units::microsecond)
+	    : m_drift_velocity(drift_velocity) {};
+	bool operator()(const IDepoPtr& lhs, const IDepoPtr& rhs) const {
+	    double t1 = lhs->time() + lhs->pos().x()/m_drift_velocity;
+	    double t2 = rhs->time() + rhs->pos().x()/m_drift_velocity;
+	    return t1 < t2;
+	}
+	double drift_velocity() { return m_drift_velocity; }
+    };
+
 }
 
 #endif
