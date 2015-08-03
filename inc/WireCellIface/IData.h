5c7345ad2f7a6a742ab21e17752a0fbeedbdbbbf
Brett Viren <brett.viren@gmail.com>
2015-08-03 17:52:22 -0400

    Clean up IData, needs applying to IWire, etc.

--- inc/WireCellIface/IData.h
+++ inc/WireCellIface/IData.h
@@ -1,13 +1,37 @@
 #ifndef WIRECELL_IDATA
 #define WIRECELL_IDATA
 
+#include "WireCellUtil/Iterator.h"
+#include <boost/range/iterator_range.hpp>
+#include <memory>		// std::shared_ptr
+
 namespace WireCell {
     template<class Type>
     class IData {
     public:
+	typedef std::shared_ptr<Type> ptr;
 	typedef std::shared_ptr<const Type> const_ptr;
+
+	typedef WireCell::Iterator<Type> iterator;
+	typedef WireCell::Iterator<const Type> const_iterator;
+
+	typedef boost::iterator_range<iterator> range;
+	typedef boost::iterator_range<const_iterator> const_range;
+
+	typedef WireCell::Iterator<ptr> ptr_iterator;
+	typedef WireCell::Iterator<const_ptr> const_ptr_iterator;
+
+	typedef boost::iterator_range<ptr_iterator> ptr_range;
+	typedef boost::iterator_range<const_ptr_iterator> const_ptr_range;
+
+	// use boost::make_iterator_range(beg,end) to make ranges
+
+
     };
 }
+
+// http://antonym.org/2014/02/c-plus-plus-11-range-based-for-loops.html
+// http://stackoverflow.com/questions/8542591/c11-reverse-range-based-for-loop
 
 
 #endif
--- inc/WireCellIface/IDepo.h
+++ inc/WireCellIface/IDepo.h
@@ -1,22 +1,20 @@
 #ifndef WIRECELL_IDEPO
 #define WIRECELL_IDEPO
 
-#include "WireCellIface/ISequence.h"
-#include "WireCellIface/ISource.h"
 #include "WireCellIface/IData.h"
-
 #include "WireCellUtil/Point.h"
 #include "WireCellUtil/Units.h"
 
-namespace WireCell {
+#include <boost/signals2.hpp>
+#include <set>
 
-    class IDepo;
-    typedef std::shared_ptr<const IDepo> IDepoPtr;
+namespace WireCell {
 
     /** An interface to information about a deposition of charge.
      */
     class IDepo : public IData<IDepo> {
     public:
+	
 	virtual ~IDepo() {};
 
 	/// The location of the deposition.
@@ -35,37 +33,25 @@ namespace WireCell {
 
     };
 
-}
-
-
-// // Access by bare "const IDepo*"
-WIRECELL_SEQUENCE_ITR(Depo,depo);
-WIRECELL_SEQUENCE_ABC(Depo,depo);
-WIRECELL_SEQUENCE_SINK(Depo,depo);
-
-// // Access by shared_ptr<const IDepo>
-WIRECELL_SEQUENCE_PTR(Depo,depo);
-WIRECELL_SOURCE_ABC(Depo,depo);
-
-namespace WireCell {
 
     /// Compare how "far" two depositions are from the origin along
     /// the drift-line (metric: dT + dX/V_drift) given a drift velocity.
-    class IDepoPtrDriftCompare {
-	double m_drift_velocity;
-    public:
+    struct IDepoPtrDriftCompare {
+	double drift_velocity;
 	IDepoPtrDriftCompare(double drift_velocity = 1.6 *units::mm/units::microsecond)
-	    : m_drift_velocity(drift_velocity) {};
-	bool operator()(const IDepoPtr& lhs, const IDepoPtr& rhs) const {
-	    double t1 = lhs->time() + lhs->pos().x()/m_drift_velocity;
-	    double t2 = rhs->time() + rhs->pos().x()/m_drift_velocity;
+	    : drift_velocity(drift_velocity) {};
+	bool operator()(const IDepo::const_ptr& lhs, const IDepo::const_ptr& rhs) const {
+	    double t1 = lhs->time() + lhs->pos().x()/drift_velocity;
+	    double t2 = rhs->time() + rhs->pos().x()/drift_velocity;
 	    if (t1 == t2) {
-		return lhs.get() < rhs.get(); // make sure no ties
+		// make sure there are no ties due to precision!
+		return lhs.get() < rhs.get(); 
 	    }
 	    return t1 < t2;
 	}
-	double drift_velocity() { return m_drift_velocity; }
     };
+    typedef std::set<IDepo::const_ptr, IDepoPtrDriftCompare> DepoTauSortedSet;
+
 
 }
 
