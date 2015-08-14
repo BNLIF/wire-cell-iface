7d5931f8d7d0e220daeddf7bc94e0a20b17b85fd
Brett Viren <bv@bnl.gov>
2015-08-14 17:46:11 -0400

    Fix interface.

--- inc/WireCellIface/ITiling.h
+++ inc/WireCellIface/ITiling.h
@@ -23,18 +23,18 @@ namespace WireCell {
 
 	/// Return a sequence of wires which are associated with the
 	/// given cell.
-	virtual IWireVector wires(const ICell::pointer& cell) const = 0;
+	virtual IWireVector wires(ICell::pointer cell) const = 0;
 	
 	/// Return a sequence of cells associated with the given wire.
-	virtual ICellVector cells(const IWire::pointer& wire) const = 0;
+	virtual ICellVector cells(IWire::pointer wire) const = 0;
 
 	/// Return the cells associated with the collection of wires.
 	/// Typically this is expected to be one wire from each plane.
 	/// No association results in an empty collection returned.
-	virtual ICellVector cell(const IWireVector& wires) const = 0;
+	virtual ICell::pointer cell(const IWireVector& wires) const = 0;
 
 	/// Return collection of nearest neighbor cells.
-	virtual ICellVector neighbors(const ICell::pointer& cell) const = 0;
+	virtual ICellVector neighbors(ICell::pointer cell) const = 0;
     };
 
 }
