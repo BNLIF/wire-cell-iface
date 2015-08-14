a4b1fa55bd8e51b83bb96d304ea23fd1fcedd332
Brett Viren <bv@bnl.gov>
2015-08-14 13:37:54 -0400

    Go back to a container based interface.

--- inc/WireCellIface/ITiling.h
+++ inc/WireCellIface/ITiling.h
@@ -1,6 +1,8 @@
 #ifndef WIRECELL_ITILING
 #define WIRECELL_ITILING
 
+#include "WireCellUtil/Interface.h"
+
 #include "WireCellIface/IWire.h"
 #include "WireCellIface/ICell.h"
 
@@ -14,23 +16,25 @@ namespace WireCell {
 
     class ITiling : virtual public Interface {
     public:
+
 	typedef std::shared_ptr<ITiling> pointer;
 	
 	virtual ~ITiling() {}
 
 	/// Return a sequence of wires which are associated with the
 	/// given cell.
-	virtual IWire::iterator_range wires(const ICell::pointer& cell) const = 0;
+	virtual IWireVector wires(const ICell::pointer& cell) const = 0;
 	
 	/// Return a sequence of cells associated with the given wire.
-	virtual ICell::iterator_range cells(const IWire::pointer& wire) const = 0;
+	virtual ICellVector cells(const IWire::pointer& wire) const = 0;
 
 	/// Return the cells associated with the collection of wires.
 	/// Typically this is expected to be one wire from each plane.
-	virtual ICell::iterator_range cell(const IWire::iterator_range& wires) const = 0;
+	/// No association results in an empty collection returned.
+	virtual ICellVector cell(const IWireVector& wires) const = 0;
 
 	/// Return collection of nearest neighbor cells.
-	virtual ICell::iterator_range neighbors(const ICell::pointer& cell) const = 0;
+	virtual ICellVector neighbors(const ICell::pointer& cell) const = 0;
     };
 
 }
