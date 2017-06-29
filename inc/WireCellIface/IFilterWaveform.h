c6a8bed2dadc8ad450a7beedf6eaed253974409a
Xin Qian <last.qx@gmail.com>
2017-06-28 22:28:32 -0400

    modify a bit

--- inc/WireCellIface/IFilterWaveform.h
+++ inc/WireCellIface/IFilterWaveform.h
@@ -11,12 +11,12 @@ namespace WireCell {
 
     class IFilterWaveform : public IComponent<IFilterWaveform> {
     public:
-        virtual IFilterWaveform() {}
+        virtual ~IFilterWaveform() {}
 
         /// Provide the filter waveform.  Note the binning of the
         /// returned waveform should be coordinated through
         /// configuration.
-        virtual const Waveform::realseq_t& filter_waveform() const = 0;
+        virtual const Waveform::realseq_t filter_waveform() const = 0;
     };
 }
 
