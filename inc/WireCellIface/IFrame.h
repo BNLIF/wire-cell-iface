feafdf80fd264778c0290273d32513ca74426db5
Brett Viren <brett.viren@gmail.com>
2017-07-27 14:51:20 -0400

    Move to tagged frame and traces.

--- inc/WireCellIface/IFrame.h
+++ inc/WireCellIface/IFrame.h
@@ -6,6 +6,9 @@
 #include "WireCellIface/WirePlaneId.h"
 #include "WireCellUtil/Waveform.h"
 
+#include <string>
+#include <vector>
+
 namespace WireCell {
 
     /** Interface to a sequence of traces.
@@ -15,8 +18,45 @@ namespace WireCell {
 
     public:
 	virtual ~IFrame() {};
-	
-	/// Return a vector of traces
+
+        // Tag gives some semantic meaning to that which it is
+        // associated.  The frame as a whole may have a number of tags
+        // as can a subset of traces.  Although they are free-form
+        // they are best kept limited in number, single words of lower
+        // case letters.  Like all data they are immutable after
+        // creation.
+        typedef std::string tag_t;
+
+        // A frame and a subset of traces may have more than one tag.
+        // Such tags are grouped into a tag list.  The order of the
+        // tag list may or may not have meaning.
+        typedef std::vector<tag_t> tag_list_t;
+
+        // A list of indices into the traces vector.
+        typedef std::vector<size_t> trace_list_t;
+
+        // A numerical summary value defined over a number of traces.
+        typedef std::vector<double> trace_summary_t;
+
+
+        // The list of tags applied to this frame as a whole.
+        virtual const tag_list_t& frame_tags() const = 0;
+
+        // The union of all tags applied to all traces of this frame.
+        virtual const tag_list_t& trace_tags() const = 0;
+
+        // Return traces (as a list of their indices) with the given
+        // tag applied.
+        virtual const trace_list_t& tagged_traces(const tag_t& tag) const = 0;
+
+        // Return a trace summary defined over the tagged subset of
+        // traces.  The values of the summary correspond to the
+        // tagged trace list as returned by `tagged_traces(tag)`.
+        virtual const trace_summary_t& trace_summary(const tag_t& tag) const = 0;
+
+
+
+	/// Return a vector of all traces ignoring any potential tag.
 	virtual ITrace::shared_vector traces() const = 0;
 
 	/// Return all masks associated with this frame
--- inc/WireCellIface/SimpleFrame.h
+++ inc/WireCellIface/SimpleFrame.h
@@ -3,7 +3,8 @@
 
 #include "WireCellIface/IFrame.h"
 #include "WireCellUtil/Units.h"
-#include <vector>
+
+#include <map>
 
 namespace WireCell {
 
@@ -25,12 +26,38 @@ namespace WireCell {
 	virtual ITrace::shared_vector traces() const;
 	virtual Waveform::ChannelMaskMap masks() const;
 
+        virtual const tag_list_t& frame_tags() const;
+        virtual const tag_list_t& trace_tags() const;
+        virtual const trace_list_t& tagged_traces(const tag_t& tag) const;
+        virtual const trace_summary_t& trace_summary(const tag_t& tag) const;
+
+
+        // Before this object is interned to a shared vector the
+        // creator may call the following non-const methods:
+
+        // Add a frame tag.
+        void tag_frame(const tag_t& tag);
+
+        // Tag a subset of traces with optional trace summary
+        void tag_traces(const tag_t& tag, const IFrame::trace_list_t& indices,
+                        const IFrame::trace_summary_t& summary = IFrame::trace_summary_t(0));
+
 
     private:
 	int m_ident;
 	double m_time, m_tick;
 	ITrace::shared_vector m_traces;
 	Waveform::ChannelMaskMap m_cmm;
+
+        IFrame::tag_list_t m_frame_tags, m_trace_tags;
+
+        struct SimpleTraceInfo {
+            IFrame::trace_list_t indices;
+            IFrame::trace_summary_t summary;
+        };
+        std::map<IFrame::tag_t, SimpleTraceInfo> m_trace_info;
+
+        const SimpleTraceInfo& get_trace_info(const IFrame::tag_t& tag) const;
     };
 
 }
