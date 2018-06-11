d2eff19c7b55ae7486f501e471ea15103b302b50
Brett Viren <brett.viren@gmail.com>
2018-06-11 16:06:45 -0400

    Use unsigned int for port numbers.

--- inc/WireCellIface/IDataFlowGraph.h
+++ inc/WireCellIface/IDataFlowGraph.h
@@ -18,7 +18,7 @@ namespace WireCell {
 	/// port number to given head port number Return false on
 	/// error.
 	virtual bool connect(INode::pointer tail, INode::pointer head,
-			     int tail_port=0, int head_port=0) = 0;
+			     size_t tail_port=0, size_t head_port=0) = 0;
 
 	/// Run the graph, return false on error.
 	virtual bool run() = 0;
